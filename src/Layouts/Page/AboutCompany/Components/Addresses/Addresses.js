8ce4148d70bd8ed18562856737af5b58d9b4cc87
Elena_O <chiffa-alenka@yandex.ru>
2022-09-08 18:21:43 +0000

    Update Addresses.js

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,14 +5,14 @@ import ListElement from "./ListElement";
 function Addresses() {
   return (
     <div className="d-inline-flex flex-column gap-3 justify-content-between mt-2 col border border-secondary rounded px-4 py-4">
-        <div className="title">Адреса заведений</div>
-        <div className="d-flex flex-column my-4 gap-3">
-          <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-          <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-          </div>
-          <div className="mt-auto gap-5 pt-5 px-1 pb-2">
-          <SecondaryButton>Добавить филиал</SecondaryButton>
-        </div>
+      <div className="title">Адреса заведений</div>
+      <div className="d-flex flex-column my-4 gap-3">
+        <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+        <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+      </div>
+      <div className="mt-auto gap-5 pt-5 px-1 pb-2">
+        <SecondaryButton>Добавить филиал</SecondaryButton>
+      </div>
     </div>
   );
 }
