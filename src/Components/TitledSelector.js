7dbe2e58b1610272ae9a40851f9a8fccc0e48f6a
5inarow <tiqoff@gmail.com>
2022-10-19 14:35:58 +0000

    Update TitledSelector.js

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -23,7 +23,7 @@ function TitledSelector({title, options}) {
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Select className='p-2'>
+            <Select className='p-4'>
                 {options.map(optionName => {
                     return (
                         <option key={optionName}>{optionName}</option>
