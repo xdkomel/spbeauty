3eafc4f61c17e9f2678c31994e3fc39c1f5971d4
Daredareos <124457171+Daredareos@users.noreply.github.com>
2023-09-11 15:36:11 +0000

    Изменение фона Primary button

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -7,7 +7,7 @@ function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
+        background: #3B82F6;
         border: 1px solid #152842;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
