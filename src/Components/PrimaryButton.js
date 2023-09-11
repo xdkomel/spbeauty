323442b1ed8a5282ddb831ac2981279cee6c3c46
Daredareos <124457171+Daredareos@users.noreply.github.com>
2023-09-11 15:37:43 +0000

    Скругление кнопки

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -8,7 +8,7 @@ function PrimaryButton(props) {
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
     const Button = styled.div`
         background: #3B82F6;
-        border: 1px solid #152842;
+        border-radius: 24px;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: white;
